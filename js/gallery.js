e48aed7163539c70c6019fce575ff3086d3d00eb
Farhan Kathawala <farhank@stanford.edu>
2015-04-29 03:59:02 -0700

    Updated gallery, social media links

--- js/gallery.js
+++ js/gallery.js
@@ -1,38 +1,56 @@
-$(document).ready(function(){
-	$("#galButton1").click(function(){
-		$imgs = $(".gallery_img");
-		$img = $imgs.filter(function(){
-		      return $(this).css('opacity') == '1';
-		})[0];
-		$imgs.each(function(index){
-			if(this == $img){
-				$($img).css('opacity', '0');
-				if(index == 0){
-					 $($imgs[43]).css('opacity', '1');
-				} else {
-				         $($imgs[index-1]).css('opacity', '1');
-				}
-				return false;
-			}
-		});
-		
-	});
+var resize = function($img, $gallery){
+    divider = ($gallery.height() - $img.height) / 2;
+    $($img).css('top', divider);
+    $($img).css('bottom', divider);
+};
 
-	$("#galButton2").click(function($imgs){
-		$imgs = $(".gallery_img");
-		$img = $imgs.filter(function(){
-		      return $(this).css('opacity') == '1';
-		})[0];
-		$imgs.each(function(index){
-			if(this == $img){
-				$($img).css('opacity', '0');
-				if(index == 43){
-					 $($imgs[0]).css('opacity', '1');
-				} else {
-				         $($imgs[index+1]).css('opacity', '1');
-				}
-				return false;
-			}
-		});
+$(window).load(function(){
+    $imgs = $(".gallery_img");
+    $img = $imgs.filter(function(){
+	return $(this).css('opacity') == '1';
+    })[0];
+    resize($img, $(".gallery"));
+});
+
+$(document).ready(function(){
+    
+    $("#galButton1").click(function(){
+	$imgs = $(".gallery_img");
+	$img = $imgs.filter(function(){
+	    return $(this).css('opacity') == '1';
+	})[0];
+	$imgs.each(function(index){
+	    if(this == $img){
+		$($img).css('opacity', '0');
+		if(index == 0){
+		    $($imgs[43]).css('opacity', '1');
+		    resize($imgs[43], $(".gallery"));
+		} else {
+		    $($imgs[index-1]).css('opacity', '1');
+		    resize($imgs[index-1], $(".gallery"));
+		}
+		return false;
+	    }
+	});	
+    });
+    
+    $("#galButton2").click(function($imgs){
+	$imgs = $(".gallery_img");
+	$img = $imgs.filter(function(){
+	    return $(this).css('opacity') == '1';
+	})[0];
+	$imgs.each(function(index){
+	    if(this == $img){
+		$($img).css('opacity', '0');
+		if(index == 43){
+		    $($imgs[0]).css('opacity', '1');
+		    resize($imgs[0], $(".gallery"));
+		} else {
+		    $($imgs[index+1]).css('opacity', '1');
+		    resize($imgs[index+1], $(".gallery"));
+		}
+		return false;
+	    }
 	});
+    });
 });
