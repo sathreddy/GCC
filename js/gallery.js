d442a0a2738b7c3fd81e119865d30b0e646607d6
Farhan Kathawala <farhank@stanford.edu>
2015-05-02 02:22:43 -0700

    Fixed gallery. Added icons for tracks

--- js/gallery.js
+++ js/gallery.js
@@ -1,32 +1,65 @@
-var resize = function($img, $gallery){
-    gwidth = $gallery.width();
-    gheight = $gallery.height();
+var pageloadwidth;
+var pageloadheight;
+var first;
+
+var resize = function($img, $gallery, first){
+    //gwidth = $gallery.width();
+    //gheight = $gallery.height();
+    if(first){
+	pageloadwidth = $gallery.width();
+	pageloadheight = $gallery.height();
+	localStorage.setItem("pwidth", pageloadwidth);
+	localStorage.setItem("pheight", pageloadheight);
+    } else {
+	$gallery.css('width', pageloadwidth);
+	$gallery.css('height', pageloadheight);
+    }
     iwidth = $img.width;
     iheight = $img.height;
-    wdivider = (gwidth - iwidth) / 2;
-    divider = (gheight - iheight) / 2;
+    wdivider = (pageloadwidth - iwidth) / 2;
+    divider = (pageloadheight - iheight) / 2;
     if(iwidth > iheight){
-	$($img).css('width', gwidth);
+	$($img).css('width', pageloadwidth);
 	$($img).css('top', divider + "px");
 	$($img).css('bottom', divider + "px");
     } else {
-	$($img).css('height', gheight);
+	$($img).css('height', pageloadheight);
 	$($img).css('left', wdivider + "px");
 	$($img).css('right', wdivider + "px");
     }
 };
 
 $(window).load(function(){
-    
+
     $imgs = $(".gallery_img");
-    var index;
-    for(index = 0; index < $imgs.size(); index++){
-	resize($imgs[index], $(".gallery"));
-    }
     $img = $imgs.filter(function(){
 	return $(this).css('opacity') == '1';
     })[0];
-    resize($img, $(".gallery"));
+    if(localStorage.getItem("first") == null){
+	console.log("first");
+	first = true;
+    } else {
+	pageloadwidth = localStorage.getItem("pwidth");
+	pageloadheight = localStorage.getItem("pheight");
+    }
+    resize($img, $(".gallery"), first);
+    localStorage.setItem("first", false);
+    first = false;
+    var index;
+    for(index = 0; index < $imgs.size(); index++){
+	resize($imgs[index], $(".gallery"), first);
+    }
+
+});
+
+$(window).resize(function(){
+
+    $imgs = $(".gallery_img");
+    $img = $imgs.filter(function(){
+	return $(this).css('opacity') == '1';
+    })[0];
+    resize($img, $(".gallery"), first);
+
 });
 
 $(document).ready(function(){
@@ -41,10 +74,10 @@ $(document).ready(function(){
 		$($img).css('opacity', '0');
 		if(index == 0){
 		    $($imgs[$imgs.size() - 1]).css('opacity', '1');
-		    resize($imgs[$imgs.size() - 1], $(".gallery"));
+		    resize($imgs[$imgs.size() - 1], $(".gallery"), first);
 		} else {
 		    $($imgs[index-1]).css('opacity', '1');
-		    resize($imgs[index-1], $(".gallery"));
+		    resize($imgs[index-1], $(".gallery"), first);
 		}
 		return false;
 	    }
@@ -62,10 +95,10 @@ $(document).ready(function(){
 		$($img).css('opacity', '0');
 		if(index == ($imgs.size() - 1)){
 		    $($imgs[0]).css('opacity', '1');
-		    resize($imgs[0], $(".gallery"));
+		    resize($imgs[0], $(".gallery"), first);
 		} else {
 		    $($imgs[index+1]).css('opacity', '1');
-		    resize($imgs[index+1], $(".gallery"));
+		    resize($imgs[index+1], $(".gallery"), first);
 		}
 		return false;
 	    }
