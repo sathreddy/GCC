c3d36217f20022a787d61281e1d10db26f75003c
Farhan Kathawala <farhank@stanford.edu>
2015-05-02 22:27:15 -0700

    Added scrolling navigation bar

---
+++ js/scroll.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+    var topOffset = $(".logo").height() - 15;
+    var aboutOffset = 0;
+    var missionOffset = $("#om").position().top;
+    var whatOffset = $("#wwb").position().top;
+    var galleryOffset = $("#gal_link").position().top + $(".mission").height();
+    var teamOffset = $("#team").position().top;
+    
+    var stickify = function(){
+	var scrollOffset = $(window).scrollTop();
+
+	if(scrollOffset > topOffset){
+	    $("#nav").addClass("sticky");
+	} else {
+	    $("#nav").removeClass("sticky");
+	}
+
+	if(scrollOffset > aboutOffset){
+	    $("#nav1").addClass("current");
+	} else {
+	    $("#nav1").removeClass("current");
+	}
+
+	if(scrollOffset > missionOffset){
+	    $("#nav2").addClass("current");
+	    $("#nav1").removeClass("current");
+	} else {
+	    $("#nav2").removeClass("current");
+	}
+	
+	if(scrollOffset > whatOffset){
+	    $("#nav3").addClass("current");
+	    $("#nav2").removeClass("current");
+	} else {
+	    $("#nav3").removeClass("current");
+	}
+
+	if(scrollOffset > galleryOffset){
+	    $("#nav4").addClass("current");
+	    $("#nav3").removeClass("current");
+	} else {
+	    $("#nav4").removeClass("current");
+	}
+
+	if(scrollOffset > teamOffset){
+	    $("#nav5").addClass("current");
+	    $("#nav4").removeClass("current");
+	} else {
+	    $("#nav5").removeClass("current");
+	}
+
+    };
+
+    $(window).scroll(function(){
+	stickify();
+    });
+    
+});
+
