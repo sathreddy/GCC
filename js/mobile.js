2520d5b1972c189b3db88891c44a0a9b56468605
Farhan Kathawala <farhank@stanford.edu>
2015-05-04 00:28:19 -0700

    Fixed mobile bugs

--- js/mobile.js
+++ js/mobile.js
@@ -4,6 +4,5 @@ $(window).load(function(){
     for(index = 0; index < $imgs.size(); index++){
 	p = $($imgs[index]).parent()
 	p.css('text-align', 'center');
-	p.parent().css('background', '#ffffff');
     }
 });
