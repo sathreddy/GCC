29b11ec7e12dae243200a242aabc5aa9f21a3f6c
Farhan Kathawala <farhank@stanford.edu>
2015-11-25 02:10:19 -0800

    Linked Hackathon and Main pages, added snazzy buttons

--- js/scroll.js
+++ js/scroll.js
@@ -1,19 +1,21 @@
 $(window).load(function(){
     //the 15 makes the transition from unsticky to sticky navbar smoother
     var topOffset = $(".logo").height() - 15;
-    var aboutOffset = 0;
+
 
     //this below corrects for the sticky navbar
     var correction = $("#nav").height() + 30;
     
     var stickify = function(){
 	var scrollOffset = $(window).scrollTop();
-
-	var missionOffset = parseInt($("#om").offset().top) - correction;
+        var aboutOffset = 0;
+        var hackOffset = parseInt($("#ghd").offset().top) - correction;
+        var missionOffset = parseInt($("#om").offset().top) - correction;
 	var whatOffset = parseInt($("#wwb").offset().top) - correction;
 	var galleryOffset = parseInt($("#gal_link").offset().top) - correction;
 	var teamOffset = parseInt($("#team").offset().top) - correction;
-
+	var contactOffset = parseInt($("#footer").offset().top) - correction;
+      
 	if(scrollOffset > topOffset){
 	    $("#nav").addClass("sticky");
 	} else {
@@ -26,43 +28,47 @@ $(window).load(function(){
 	    $("#nav1").removeClass("current");
 	}
 
-	if(scrollOffset > missionOffset){
+	if(scrollOffset > hackOffset){
 	    $("#nav2").addClass("current");
 	    $("#nav1").removeClass("current");
 	} else {
 	    $("#nav2").removeClass("current");
 	}
 	
-	if(scrollOffset > whatOffset){
+	if(scrollOffset > missionOffset){
 	    $("#nav3").addClass("current");
 	    $("#nav2").removeClass("current");
 	} else {
 	    $("#nav3").removeClass("current");
 	}
 
-	if(scrollOffset > galleryOffset){
+	if(scrollOffset > whatOffset){
 	    $("#nav4").addClass("current");
 	    $("#nav3").removeClass("current");
 	} else {
 	    $("#nav4").removeClass("current");
 	}
 
-	if(scrollOffset > teamOffset){
+	if(scrollOffset > galleryOffset){
 	    $("#nav5").addClass("current");
 	    $("#nav4").removeClass("current");
 	} else {
 	    $("#nav5").removeClass("current");
 	}
 
-	//we're actually detecting if the user is at the bottom of the page
-	//here. Contact info is too far down to reach by scrolling
-	if(scrollOffset + $(window).height() == $(document).height()){
+	if(scrollOffset > teamOffset){
 	    $("#nav6").addClass("current");
 	    $("#nav5").removeClass("current");
 	} else {
 	    $("#nav6").removeClass("current");
 	}
 
+	if(scrollOffset > contactOffset){
+	    $("#nav7").addClass("current");
+	    $("#nav6").removeClass("current");
+	} else {
+	    $("#nav7").removeClass("current");
+	}
     };
 
     $(window).scroll(function(){
