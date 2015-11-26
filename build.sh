78af5bd2f24f164a292b7935adad0666606ff78b
Farhan Kathawala <farhank@stanford.edu>
2015-11-26 11:15:01 -0800

    Fixed hover colors, some stray HTML, added build script

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+mkdir build
+
+rsync -av --progress ./ build/ --exclude build
+
+cd build
+
+for file in *.html; do
+    sed -E '/(.min.js)/!s/\.js/.min.js/g' "$file" > "$file".tmp && mv "$file".tmp "$file"
+    sed -E '/(.min.css)/!s/\.css/.min.css/g' "$file" > "$file".tmp && mv "$file".tmp "$file"
+    # mv "$file" "$(basename ${file} .html).htm"
+done
+
+rm *.md && rm *.txt && rm *.sh
+
+# css-html-js-minify.py $(realpath ./) && rm *.htm
+
+cd js && css-html-js-minify.py $(realpath ./)
+
+cd ../css && css-html-js-minify.py $(realpath ./)
